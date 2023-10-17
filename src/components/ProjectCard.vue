26d21de2bdc06a179196cc4e3f122eec13055b98
PasqualeIsabelli <pasquale0395@gmail.com>
2023-10-17 14:20:39 +0200

    fix some details

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -34,17 +34,17 @@
 <template>
   <div class="row row-cols-6 mt-4 gy-4">
     <div class="col" v-for="project in projects" :key="project.id">
-        <div class="card p-0">
-          <img :src="getImageUrl(project)" class="card-img-top">
-          <div class="card-body ">
-            <h5 class="card-title">{{ project.title }}</h5>
-            <div class="d-flex align-items-center gap-2">
-              <span class="badge bg-secondary">{{ project.type.type }}</span>
-              <p v-for="tech in project.technologies" class="badge m-0" :style="`background-color: rgb(${ tech.color })`"><i :class="`fa-brands ${ tech.name }`"></i></p>
-            </div>
+      <div class="card p-0 h-100">
+        <img :src="getImageUrl(project)" class="card-img-top">
+        <div class="card-body ">
+          <h5 class="card-title">{{ project.title }}</h5>
+          <div class="d-flex align-items-center gap-2">
+            <span class="badge bg-secondary">{{ project.type.type }}</span>
+            <p v-for="tech in project.technologies" class="badge m-0" :style="`background-color: rgb(${ tech.color })`"><i :class="`fa-brands ${ tech.name }`"></i></p>
           </div>
         </div>
       </div>
+    </div>
   </div>
   <div class="d-flex justify-content-center mt-4">
     <a v-for="singlePage in pagination.links" class="btn btn-link" @click="fetchData(singlePage.url)" v-html="singlePage.label"></a>
